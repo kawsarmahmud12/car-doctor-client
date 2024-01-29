9016e03fe21cae963e4e07fc38061801223bab7c
kawsarmahmud12 <kawsarmahmud204@gmail.com>
2024-01-29 23:16:23 +0600

    file upload

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,18 +1,28 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home/Home";
+import Login from "../Pages/Login/Login";
+import About from "../Pages/Home/About/About";
 
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Main></Main>,
-      children:[
-        {
-            path: '/',
-            element:<Home></Home>
-        }
-      ]
-    },
-  ]);
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path:'/about',
+        element:<About></About>
+      },
+      {
+        path:'/login',
+        element:<Login></Login>
+      }
+    ]
+  },
+]);
 
-  export default router
+export default router
